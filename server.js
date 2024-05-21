37a8041bdea867b6f604c068d71275b6bed86e7a
Tarek <tael3424@th-wildau.de>
2024-05-21 11:53:10 +0200

    server created

---
+++ server.js
@@ -0,0 +1,10 @@
+import http from "http";
+const PORT = 8000;
+
+const requestListener = function (req, res) {
+  res.writeHead(418);
+  res.end("Hello World");
+};
+
+const server = http.createServer(requestListener);
+server.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
