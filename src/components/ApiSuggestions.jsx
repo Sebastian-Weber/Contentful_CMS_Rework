cc4df8c70821757f0d5e2e8c58a385119d3751b6
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-05-23 00:24:29 +0200

    added slider

--- src/components/ApiSuggestions.jsx
+++ src/components/ApiSuggestions.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import useFetchData from "./FetchData";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function ApiSuggestions({ category }) {
   const { entries } = useFetchData();
@@ -8,47 +11,51 @@ function ApiSuggestions({ category }) {
   const filteredEntries = entries.filter(
     (entry) => entry.category === category
   );
-
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: true,
+    // centerPadding: "100px",
+  };
   return (
     <>
       <h3 className="pl-2 text-base leading-5 font-bold tracking-tight text-slate-300 ">
         other APIs from the category {category}:
       </h3>
-      <div className="flex my-5">
-        {filteredEntries.map((entry) => (
-          <div
-            key={entry.id}
-            className="flex w-80 p-6 mx-1 pb-2 bg-slate-700 border border-slate-700 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
-          >
-            <img className="h-12 pt-0" src={entry.url} alt={entry.title} />
-            <h3 className="pl-2 text-base leading-5 font-bold tracking-tight text-slate-300 ">
-              {entry.title}
-            </h3>
-            <Link to={`/API-details/${entry.id}`}>
-              <div className="flex flex-row justify-end pb-2">
-                <div className="flex flex-row">
-                  <button
-                    id="toggle-btn"
-                    className="mt-4 text-sm text-blue-500 focus:outline-none"
-                  >
-                    Read More
-                  </button>
-                  <div className="pt-4 px-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="24px"
-                      viewBox="0 -960 960 960"
-                      width="24px"
-                      fill="#3b82f6"
-                    >
-                      <path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z" />
-                    </svg>
+      <div className="my-5">
+        <Slider {...settings}>
+          {filteredEntries.map((entry) => (
+            <Link to={`/API-details/${entry.id}`} key={entry.id}>
+              <div className="flex flex-col w-60 p-3 mx-1 pb-2 bg-slate-700 border border-slate-700 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
+                <div className="flex">
+                  <img
+                    className="h-10 pt-0"
+                    src={entry.file.url}
+                    alt={entry.title}
+                  />
+                  <div className="flex flex-col">
+                    <h3 className="pl-2 text-base leading-5 font-bold tracking-tight text-slate-300 ">
+                      {entry.title}
+                    </h3>
+                    <p className="pl-2 font-normal text-slate-300 dark:text-gray-400 ">
+                      by {entry.developer}
+                    </p>
                   </div>
                 </div>
+
+                <button
+                  id="toggle-btn"
+                  className="mt-4 text-sm text-blue-500 focus:outline-none"
+                >
+                  Read More
+                </button>
               </div>
             </Link>
-          </div>
-        ))}
+          ))}
+        </Slider>
       </div>
     </>
   );
